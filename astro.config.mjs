419c98c44afc7cfe78552b699279ed4f332cf3f0
Zeq <Ezequiellopez.dev@gmail.com>
2023-12-18 15:47:36 -0300

    Spotify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import svelte from '@astrojs/svelte'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), react(), svelte()],
+	output: 'server',
+	adapter: vercel(),
+})
